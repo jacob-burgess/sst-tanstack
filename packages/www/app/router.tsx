cafb83b79cae3e8932420d2b3c82eb2aecf574e4
Jacob Burgess <jacobburgess11@gmail.com>
2024-09-27 17:42:56 -0700

    missed one

--- packages/www/app/router.tsx
+++ packages/www/app/router.tsx
@@ -1,7 +1,7 @@
 import { QueryClient } from "@tanstack/react-query";
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import { routerWithQueryClient } from "@tanstack/react-router-with-query";
-import { DefaultCatchBoundary } from "~/components/DefaultCatchBoundary";
+import { DefaultCatchBoundary } from "~/components/default-catch-boundary";
 import { NotFound } from "~/components/not-found";
 import { routeTree } from "~/routeTree.gen";
 
