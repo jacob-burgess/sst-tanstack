5e4f1f17b492bafec8bcbdeb22187272e17b0c7e
Jacob Burgess <jacobburgess11@gmail.com>
2024-10-16 16:31:11 -0700

    remove seed infra

--- infra/massaaage-vectors.ts
+++ infra/massaaage-vectors.ts
@@ -1,37 +1,41 @@
-import { database } from "./database";
+// import { database } from "./database";
 
-const dlq = new sst.aws.Queue("MassaaageVectorsDLQ");
+// if (!process.env.OPENAI_API_KEY) {
+//   throw new Error("OPENAI_API_KEY is not set");
+// }
 
-const massaaageQ = new sst.aws.Queue("MassaaageVectorsQ", {
-  dlq: dlq.arn,
-  transform: {
-    queue(args, opts, name) {
-      args.visibilityTimeoutSeconds = 2 * 60; // 2 minutes
-    },
-  },
-});
+// const dlq = new sst.aws.Queue("MassaaageVectorsDLQ");
 
-massaaageQ.subscribe({
-  handler: "./packages/functions/src/massaaage.handler",
-  timeout: "2 minutes",
-  memory: "2 GB",
-  link: [database],
-  environment: {
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-  },
-});
+// const massaaageQ = new sst.aws.Queue("MassaaageVectorsQ", {
+//   dlq: dlq.arn,
+//   transform: {
+//     queue(args, opts, name) {
+//       args.visibilityTimeoutSeconds = 2 * 60; // 2 minutes
+//     },
+//   },
+// });
 
-export const publisher = new sst.aws.Function("MassaaageVectorsPublisher", {
-  handler: "./packages/functions/src/publisher.handler",
-  url: true,
-  link: [massaaageQ, database],
-  timeout: "5 minutes",
-  environment: {
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-  },
-});
+// massaaageQ.subscribe({
+//   handler: "./packages/functions/src/massaaage.handler",
+//   timeout: "2 minutes",
+//   memory: "2 GB",
+//   link: [database],
+//   environment: {
+//     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+//   },
+// });
 
-export const outputs = {
-  massaaageQ: massaaageQ.url,
-  publisher: publisher.url,
-};
+// export const publisher = new sst.aws.Function("MassaaageVectorsPublisher", {
+//   handler: "./packages/functions/src/publisher.handler",
+//   url: true,
+//   link: [massaaageQ, database],
+//   timeout: "5 minutes",
+//   environment: {
+//     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+//   },
+// });
+
+// export const outputs = {
+//   massaaageQ: massaaageQ.url,
+//   publisher: publisher.url,
+// };
